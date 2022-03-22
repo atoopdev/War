b36d9ef450da5348c38f3fd60fef591a000ed78f
Amanda Toop <60271855+atoopdev@users.noreply.github.com>
2022-03-22 11:55:14 -0700

    moved example code to own file

--- examples.js
+++ examples.js
@@ -1,22 +1,3 @@
-
-document.getElementById("newdeck").addEventListener("click", getNewDeck)
-// get deck of cards
-
-function getNewDeck(){
-    console.log("Button clicked")
-fetch("https://apis.scrimba.com/deckofcards/api/deck/new/shuffle/", {method: "GET"})
-.then(response => response.json())
-.then(data =>{
-    console.log(data)
-})
-}
-
-setTimeout(callBack, 2000)
-
-function callBack(){
-    console.log("I finally ran!")
-}
-
 // ----------------------------------------------------------------------------------
 
 const people = [
