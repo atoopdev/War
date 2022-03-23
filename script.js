2291816cfb0c9dcf75169b8c7ebc67ea05c80c93
Amanda Toop <60271855+atoopdev@users.noreply.github.com>
2022-03-23 10:52:59 -0700

    get deck id

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 
 document.getElementById("newdeck").addEventListener("click", getNewDeck)
 // get deck of cards
+let deck_id;
 
 function getNewDeck(){
     console.log("Button clicked")
@@ -8,14 +9,18 @@ fetch("https://apis.scrimba.com/deckofcards/api/deck/new/shuffle/", {method: "GE
 .then(response => response.json())
 .then(data =>{
     console.log(data)
+    deck_id = data.deck_id
+    console.log("Deck id: ", deck_id)
 })
 }
 
-setTimeout(callBack, 2000)
 
-function callBack(){
-    console.log("I finally ran!")
-}
+
+// setTimeout(callBack, 2000)
+
+// function callBack(){
+//     console.log("I finally ran!")
+// }
 
 // promise - statement of what will happen - you will get a response
 // promise states
