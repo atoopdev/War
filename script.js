279938d4d7b8a77b150a5d0e0eb4c430f8f97754
Amanda Toop <60271855+atoopdev@users.noreply.github.com>
2022-03-23 13:32:12 -0700

    render card img to screen

--- script.js
+++ script.js
@@ -21,9 +21,9 @@ fetch("https://apis.scrimba.com/deckofcards/api/deck/new/shuffle/", {method: "GE
 function drawCards(){
     // check for deck id - new deck button has been clicked
     if(deck_id){
-    console.log("Drawing cards")
-    console.log("deck id in draw cards: ", deck_id)
-    console.log(`https://apis.scrimba.com/deckofcards/api/deck/${deck_id}/draw/?count=${numCards}`)
+    // console.log("Drawing cards")
+    // console.log("deck id in draw cards: ", deck_id)
+    // console.log(`https://apis.scrimba.com/deckofcards/api/deck/${deck_id}/draw/?count=${numCards}`)
     fetch(`https://apis.scrimba.com/deckofcards/api/deck/${deck_id}/draw/?count=2`, {method: "GET"})
     .then(response => response.json())
     .then(data =>{
@@ -33,10 +33,27 @@ function drawCards(){
         }
         console.log("Cards array: ", cards)
     })
+    setTimeout(renderCards, 3000)
 }
 else{
     console.log("Need new deck")
 }
+}
+
+function renderCards(){
+    let cardsHTML = ""
+    if(cards){
+    console.log("Rendering cards in renderCards: ", cards)
+    console.log("Cards length: ", cards.length)
+    for(let i=0;i<2;i++){
+        console.log("In for loop")
+         cardsHTML+=
+            `
+            <img src="${cards[i].image}"/>`
+            console.log("Cardshtml in for loop", cardsHTML)
+    }
+    document.getElementById("cardsEL").innerHTML = cardsHTML
+    }
 }
 
 // setTimeout(callBack, 2000)
