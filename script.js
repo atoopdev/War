1b99d43ff8b0d4c2c961855dd8a47e62dfb8588d
Amanda Toop <60271855+atoopdev@users.noreply.github.com>
2022-03-23 11:40:44 -0700

    Draw 2 cards and save to array

--- script.js
+++ script.js
@@ -1,7 +1,11 @@
-
-document.getElementById("newdeck").addEventListener("click", getNewDeck)
-// get deck of cards
 let deck_id;
+let cards = []
+const numCards = 2
+
+// get deck of cards
+document.getElementById("newdeck").addEventListener("click", getNewDeck)
+// draw cards
+document.getElementById("drawcards").addEventListener("click", drawCards)
 
 function getNewDeck(){
     console.log("Button clicked")
@@ -10,11 +14,30 @@ fetch("https://apis.scrimba.com/deckofcards/api/deck/new/shuffle/", {method: "GE
 .then(data =>{
     console.log(data)
     deck_id = data.deck_id
-    console.log("Deck id: ", deck_id)
+    console.log("Deck id: ", deck_id) 
 })
 }
 
-
+function drawCards(){
+    // check for deck id - new deck button has been clicked
+    if(deck_id){
+    console.log("Drawing cards")
+    console.log("deck id in draw cards: ", deck_id)
+    console.log(`https://apis.scrimba.com/deckofcards/api/deck/${deck_id}/draw/?count=${numCards}`)
+    fetch(`https://apis.scrimba.com/deckofcards/api/deck/${deck_id}/draw/?count=2`, {method: "GET"})
+    .then(response => response.json())
+    .then(data =>{
+        console.log("Drawn cards: ", data)
+        for(let card of data.cards){
+            cards.push(card)
+        }
+        console.log("Cards array: ", cards)
+    })
+}
+else{
+    console.log("Need new deck")
+}
+}
 
 // setTimeout(callBack, 2000)
 
