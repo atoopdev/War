1e76063cb320f39fe20ada7689be24e54f9458cf
Amanda Toop <60271855+atoopdev@users.noreply.github.com>
2022-04-07 09:59:43 -0700

    remove console.logs

--- script.js
+++ script.js
@@ -14,13 +14,10 @@ document.getElementById("drawcards").addEventListener("click", drawCards)
 
 
 function getNewDeck(){
-    // console.log("Button clicked")
 fetch("https://apis.scrimba.com/deckofcards/api/deck/new/shuffle/", {method: "GET"})
 .then(response => response.json())
 .then(data =>{
-    // console.log(data)
     deck_id = data.deck_id
-    // console.log("Deck id: ", deck_id)
     document.getElementById("remainingcards").innerText = `Remaining cards: ${data.remaining}` 
 })
 deck_done = false
@@ -30,18 +27,14 @@ enableDrawCards()
 function drawCards(){
     // check for deck id - new deck button has been clicked
     if(deck_id&&deck_done===false){
-    // console.log("Drawing cards")
-    // console.log("deck id in draw cards: ", deck_id)
     fetch(`https://apis.scrimba.com/deckofcards/api/deck/${deck_id}/draw/?count=2`, {method: "GET"})
     .then(response => response.json())
     .then(data =>{
-        // console.log("Drawn cards: ", data)
         document.getElementById("remainingcards").innerText = `Remaining cards: ${data.remaining}`
         for(let card of data.cards){
             // changing from pop to allow new cards dealt to auto appear
             cards.unshift(card)
         }
-        // console.log("Cards array: ", cards)
     renderCards()
         // if no cards left
     if(data.remaining===0){
@@ -73,25 +66,19 @@ function determineWinner(){
 
 function disableDrawCards(){
     document.getElementById("drawcards").style.display = "none"
-    // document.getElementById("drawcards").button.disabled = true
 }
 
 function enableDrawCards(){
     document.getElementById("drawcards").style.display = "block"
-    // document.getElementById("drawcards").button.disabled = false
 
 }
 
 function renderCards(){
     let cardsHTML = ""
     if(cards){
-    // console.log("Rendering cards in renderCards: ", cards)
-    // console.log("Cards length: ", cards.length)
     for(let i=0;i<numCards;i++){
-        // console.log("In for loop")
          cardsHTML+=
             `<img src="${cards[i].image}"/>`
-            // console.log("Cardshtml in for loop", cardsHTML)
     }
     document.getElementById("cardsEL").innerHTML = cardsHTML
     }
@@ -102,23 +89,18 @@ function renderCards(){
 
 function determineScore(card){
     if(card <=10){
-        // console.log(`Card is: ${card} value is ${Number(card)}`)
         return Number(card)
     }
     if(card==="JACK"){
-        // console.log(`Card is: ${card} returning 11`)
         return 11
     }
     if(card==="QUEEN"){
-        // console.log(`Card is: ${card} returning 12`)
         return 12
     }
     if(card==="KING"){
-        // console.log(`Card is: ${card} returning 13`)
         return 13
     }
     else{
-        // console.log(`Card is: ${card} returning 14`)
         return 14
     }
 }
@@ -129,32 +111,23 @@ function determineHigherScore(card1, card2){
     // const card1ValueIndex = arrayofcards.indexOf(card1.value)
     // const card2ValueIndex = arrayofcards.indexOf(card2.value)
     const card1_score = determineScore(card1)
-    // console.log("Card1_score: ", card1_score)
     document.getElementById("computerscore").innerText= `Computer: ${card1_score} Score: ${computer_score}`
     const card2_score = determineScore(card2)
-    // console.log("Card2_score: ", card2_score)
     document.getElementById("myscore").innerText= `Me: ${card2_score} Score: ${user_score}`
     if(card1_score===card2_score){
-        // console.log("cards equal")
         return "Its a tie! War!"
     }else if(card1_score>card2_score){
-            // console.log("Card 1 has larger score")
             computer_score+=1
             document.getElementById("computerscore").innerText= `Computer: ${card1_score} Score: ${computer_score}`
             return "Computer wins!"
         }else{
-            // console.log("card 2 has larger score")
             user_score+=1
             document.getElementById("myscore").innerText= `Me: ${card2_score} Score: ${user_score}`
             return "You win!"
         }
 }
 
-// setTimeout(callBack, 2000)
 
-// function callBack(){
-//     console.log("I finally ran!")
-// }
 
 // promise - statement of what will happen - you will get a response
 // promise states
